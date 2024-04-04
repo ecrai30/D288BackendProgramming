b63cbe3cd1621bf541292249ace76b67f35e6708
Admin <ecrai30@wgu.edu>
2024-04-04 09:42:31 -0700

    Added Division.java entity

---
+++ src/main/java/com/example/demo/entities/Divison.java
@@ -0,0 +1,37 @@
+package com.example.demo.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.Set;
+
+@Table(name = "divisions")
+@Entity
+@Getter
+@Setter
+public class Divison {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "division_id")
+    private Long id;
+
+    @Column(name = "division")
+    private String division_name;
+
+    @Column(name = "create_date")
+    private Date create_date;
+
+    @Column(name = "last_update")
+    private Date last_update;
+
+    @Column(name = "country_id")
+    private Long country_id;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy =  "category")
+    private Set<Customer> customers;
+
+
+}
