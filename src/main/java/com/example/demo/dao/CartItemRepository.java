4917c83bddbd1ae85fcc7ea81901bf2c5ec03b19
Admin <ecrai30@wgu.edu>
2024-04-05 12:33:30 -0700

    Added CrossOrigin to Repo

--- src/main/java/com/example/demo/dao/CartItemRepository.java
+++ src/main/java/com/example/demo/dao/CartItemRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.dao;
 
 import com.example.demo.entities.CartItem;
+import jakarta.persistence.Column;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
--- src/main/java/com/example/demo/dao/CartRepository.java
+++ src/main/java/com/example/demo/dao/CartRepository.java
@@ -2,9 +2,10 @@ package com.example.demo.dao;
 
 import com.example.demo.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.math.BigInteger;
-
+@CrossOrigin
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
 }
