b3c7ef4d16a32711aa75a918295c0813b2dee57d
Admin <ecrai30@wgu.edu>
2024-04-05 08:39:45 -0700

    Updated Enum

--- src/main/java/com/example/demo/entities/StatusType.java
+++ src/main/java/com/example/demo/entities/StatusType.java
@@ -1,5 +1,11 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+
 public enum StatusType {
+    pending, ordered, canceled
 
 }
