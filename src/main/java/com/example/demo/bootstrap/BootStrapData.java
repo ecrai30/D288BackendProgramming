b9d70c748348f59543d3495c1733002e6a4091c1
Admin <ecrai30@wgu.edu>
2024-04-11 13:14:24 -0700

    Added if statement to prevent duplicating customers and divisions

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -20,52 +20,63 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        if(customerRepository.count() > 1 || divisionRepository.findById(7L).isEmpty()) {
+            return;
+        }
 
-    //Divisions
-    Division division = divisionRepository.findById(7L).orElse(null);
+        else{
 
-    //Customers
-    Customer melissa = new Customer();
-    melissa.setFirstName("Melissa");
-    melissa.setLastName("Craig");
-    melissa.setAddress("342 Shelton Lane");
-    melissa.setPostal_code("36598");
-    melissa.setPhone("2569847516");
-    melissa.setDivision(division);
-    customerRepository.save(melissa);
+                //Divisions//Customers
+                Division division1 = divisionRepository.findById(7L).orElse(null);
+                Customer melissa = new Customer();
+                melissa.setFirstName("Melissa");
+                melissa.setLastName("Craig");
+                melissa.setAddress("342 Shelton Lane");
+                melissa.setPostal_code("36598");
+                melissa.setPhone("2569847516");
+                melissa.setDivision(division1);
+                customerRepository.save(melissa);
 
-    Customer leticia = new Customer();
-    leticia.setFirstName("Leticia");
-    leticia.setLastName("Marcela");
-    leticia.setAddress("342 Brazil Lane");
-    leticia.setPostal_code("26598");
-    leticia.setPhone("2569877516");
-    customerRepository.save(leticia);
+                Division division2 = divisionRepository.findById(26L).orElse(null);
+                Customer leticia = new Customer();
+                leticia.setFirstName("Leticia");
+                leticia.setLastName("Marcela");
+                leticia.setAddress("342 Brazil Lane");
+                leticia.setPostal_code("26598");
+                leticia.setPhone("2569877516");
+                leticia.setDivision(division2);
+                customerRepository.save(leticia);
 
-    Customer christina = new Customer();
-    christina.setFirstName("Christina");
-    christina.setLastName("Craig");
-    christina.setAddress("342 Mills Lane");
-    christina.setPostal_code("36498");
-    christina.setPhone("2569847556");
-    customerRepository.save(christina);
+                Division division3 = divisionRepository.findById(39L).orElse(null);
+                Customer christina = new Customer();
+                christina.setFirstName("Christina");
+                christina.setLastName("Craig");
+                christina.setAddress("342 Mills Lane");
+                christina.setPostal_code("36498");
+                christina.setPhone("2569847556");
+                christina.setDivision(division3);
+                customerRepository.save(christina);
 
-    Customer richard = new Customer();
-    richard.setFirstName("Richard");
-    richard.setLastName("Craig");
-    richard.setAddress("342 Lowry Lane");
-    richard.setPostal_code("36098");
-    richard.setPhone("2569847514");
-    customerRepository.save(richard);
+                Division division4 = divisionRepository.findById(6L).orElse(null);
+                Customer richard = new Customer();
+                richard.setFirstName("Richard");
+                richard.setLastName("Craig");
+                richard.setAddress("342 Lowry Lane");
+                richard.setPostal_code("36098");
+                richard.setPhone("2569847514");
+                richard.setDivision(division4);
+                customerRepository.save(richard);
 
-    Customer victor = new Customer();
-    victor.setFirstName("Victor");
-    victor.setLastName("Craig");
-    victor.setAddress("342 Mills Lane");
-    victor.setPostal_code("34598");
-    victor.setPhone("2569647516");
-    customerRepository.save(victor);
-
-
+                Division division5 = divisionRepository.findById(10L).orElse(null);
+                Customer victor = new Customer();
+                victor.setFirstName("Victor");
+                victor.setLastName("Craig");
+                victor.setAddress("342 Mills Lane");
+                victor.setPostal_code("34598");
+                victor.setPhone("2569647516");
+                victor.setDivision(division5);
+                customerRepository.save(victor);
+            }
+        }
     }
-}
+
